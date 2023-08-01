2a0a45f63175fae1113d3d33d04545a052be029e
Ashin K J <ashinkj06@gmail.com>
2023-08-01 23:21:48 +0530

    render data in cart and checkout page

--- store/views.py
+++ store/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from .models import *
 
-
-
-
 def store(request):
     products=Product.objects.all()
     context = {
@@ -12,7 +9,34 @@ def store(request):
     return render(request,"store/store.html",context)
 
 def cart(request):
-    return render(request,"store/cart.html")
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created=Order.objects.get_or_create(customer=customer,complete=False)
+        items=order.orderitem_set.all()
+    else:
+        items=[]
+        order={'get_cart_total':0,'get_cart_items':0}
+
+
+    context ={
+        'items':items,
+        'order':order
+    }
+    return render(request,"store/cart.html",context)
 
 def checkout(request):
-    return render(request,"store/checkout.html")
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created=Order.objects.get_or_create(customer=customer,complete=False)
+        items=order.orderitem_set.all()
+    else:
+        items=[]
+        order={'get_cart_total':0,'get_cart_items':0}
+
+
+    context ={
+        'items':items,
+        'order':order
+    }
+
+    return render(request,"store/checkout.html",context)
