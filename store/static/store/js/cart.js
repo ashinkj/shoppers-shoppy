42f40be32024a8b2ac9c14d2301532c8bf80ce5b
Ashin K J <ashinkj06@gmail.com>
2023-08-02 23:25:08 +0530

    update the user's orders using fetch and promise functions

---
+++ store/static/store/js/cart.js
@@ -0,0 +1,48 @@
+var updateBtns=document.getElementsByClassName("update-cart")
+
+for(var i=0;i < updateBtns.length;i++){
+    updateBtns[i].addEventListener("click",function(){
+        var productId = this.dataset.product
+        var action =this.dataset.action
+        console.log('productId:',productId,'action:',action);
+ 
+        console.log("USER:",user)
+        if(user === 'AnonymousUser'){
+            console.log("not logged in")
+        }
+        else{
+            updateUserOrder(productId,action)
+        }
+    })
+
+}
+
+function updateUserOrder(productId,action){
+    console.log('user is logged in')
+    var url ='/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log('data:', data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+          console.log('Response status:', response.status);
+          console.log('Response text:', response.statusText);
+          return response.text(); // Log the response content
+        });
+      
+}
--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("",views.store,name="store-page"),
     path("addcart/",views.cart,name="cart-page"),
     path("checkout/",views.checkout,name="checkout-page"),
+    path("update_item/",views.updateItem,name="update-item"),
 
 ]
