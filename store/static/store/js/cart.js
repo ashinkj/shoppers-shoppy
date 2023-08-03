af5f6f54775a71e4aa605629fefd4b41ced94219
Ashin K J <ashinkj06@gmail.com>
2023-08-03 13:10:49 +0530

    Total amount and there quantity changes according to the increment and decrement button in chart page called

--- store/static/store/js/cart.js
+++ store/static/store/js/cart.js
@@ -37,6 +37,7 @@ function updateUserOrder(productId,action){
         })
         .then((data) => {
           console.log('data:', data);
+          location.reload()
         })
         .catch((error) => {
           console.error('Error fetching data:', error);
--- store/views.py
+++ store/views.py
@@ -4,6 +4,16 @@ from .models import *
 import json
 
 def store(request):
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created=Order.objects.get_or_create(customer=customer,complete=False)
+        items=order.orderitem_set.all()
+        cartItems=order.get_cart_items
+    else:
+        items=[]
+        order={'get_cart_total':0,'get_cart_items':0}
+        cartItems=order['get_cart_items']
+
     products=Product.objects.all()
     context = {
         'products':products
