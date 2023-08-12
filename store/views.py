bdebad1d0633a934538997f67d4aa58330f6b0ea
Ashin K J <ashinkj06@gmail.com>
2023-08-12 23:58:55 +0530

    guestuser data is added to the db when checkout

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from .models import *
 import json
 import datetime
-from .utils import cookieCart,cartData
+from .utils import cookieCart,cartData,guestOrder
 
 def store(request):
     
@@ -76,25 +76,25 @@ def processOrder(request):
         if request.user.is_authenticated:
             customer = request.user.customer
             order, created =Order.objects.get_or_create(customer=customer,complete=False)
-            total = float(data['form']['total'])
-            order.transaction_id = transaction_id
-
-            if total == order.get_cart_total:
-                order.complete = True
-            order.save()
-                
-            if order.shipping:
-                shipping_address = ShippingAddress(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-            )
-            shipping_address.save()
-
+         
         else:
-            print("user is not logged in...")
+           customer,order = guestOrder(request,data)
+
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == float(order.get_cart_items):
+            order.complete = True
+        order.save()
+
+        if order.shipping:
+            shipping_address = ShippingAddress(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+    )
 
         return JsonResponse("payment complete",safe=False)
