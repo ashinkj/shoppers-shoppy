afceac32aef27a80144225868692664660e86db8
Ashin K J <ashinkj06@gmail.com>
2023-09-17 20:02:24 +0530

    password reset via email

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
     path('login/', user_views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('activate/<uidb64>/<token>/',user_views.activate, name='activate'),  
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "user/reset_password.html"), name ='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "user/password_reset_sent.html"), name ='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "user/password_reset_form.html"), name ='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "user/password_reset_done.html"), name ='password_reset_complete'),
     path('',include('store.urls')),
     
 ]
